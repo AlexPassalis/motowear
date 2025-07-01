69012b9060705897df142b82c53029834613b63a
AlexPassalis <alexpassalis9@gmail.com>
2025-07-01 11:59:41 +0300

    prevent dublicate pageViews fom Facebook Pixel

--- services/nextjs/src/lib/facebook-pixel/FacebookPixel.tsx
+++ services/nextjs/src/lib/facebook-pixel/FacebookPixel.tsx
@@ -1,14 +1,19 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { facebookPixelPageView } from '@/lib/facebook-pixel/index'
 
 export function FacebookPixel() {
   const pathname = usePathname()
+  const firstRun = useRef(true)
 
   useEffect(() => {
-    facebookPixelPageView()
+    if (firstRun.current) {
+      firstRun.current = false
+      return
+    }
+    facebookPixelPageView() // fires on real route changes
   }, [pathname])
 
   return null
