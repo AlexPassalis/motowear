01e52f4e8cb8d825227f27e5245c09589e67c31c
AlexPassalis <alexpassalis9@gmail.com>
2025-08-05 12:25:42 +0200

    Moving away from motowear.gr.com to motowear.gr

--- services/nextjs/src/utils/OPTIONS.ts
+++ services/nextjs/src/utils/OPTIONS.ts
@@ -4,7 +4,7 @@ export async function OPTIONS() {
   return new NextResponse(null, {
     status: 204, // No Content
     headers: {
-      'Access-Control-Allow-Origin': 'https://www.motowear.gr.com',
+      'Access-Control-Allow-Origin': 'https://www.motowear.gr',
       'Access-Control-Allow-Methods': 'POST, OPTIONS',
       'Access-Control-Allow-Headers': 'Content-Type, Authorization',
       'Access-Control-Allow-Credentials': 'true',
