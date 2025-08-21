13919dfb21ff665c270a144b813ca4b07d997262
AlexPassalis <alexpassalis9@gmail.com>
2025-08-21 15:35:28 +0200

    Removed tags to revert a bug that went to production, working on fixing it.

--- services/nextjs/src/app/(user)/product/[[...params]]/page.tsx
+++ services/nextjs/src/app/(user)/product/[[...params]]/page.tsx
@@ -8,10 +8,10 @@ import {
 } from '@/app/(user)/cache'
 import { notFound, redirect } from 'next/navigation'
 import { ROUTE_ERROR } from '@/data/routes'
-import { Metadata } from 'next'
+// import { Metadata } from 'next'
 import { specialVariant } from '@/data/magic'
-import { errorPostgres } from '@/data/error'
-import { envServer } from '@/env'
+// import { errorPostgres } from '@/data/error'
+// import { envServer } from '@/env'
 
 type typeParams = { params?: [type?: string, version?: string] }
 type typeSearchParams = { color?: string }
@@ -20,119 +20,119 @@ type ProductPageProps = {
   searchParams: Promise<typeSearchParams>
 }
 
-export async function generateMetadata({
-  params,
-  searchParams,
-}: ProductPageProps): Promise<Metadata> {
-  const resolvedPageProps = await Promise.allSettled([params, searchParams])
+// export async function generateMetadata({
+//   params,
+//   searchParams,
+// }: ProductPageProps): Promise<Metadata> {
+//   const resolvedPageProps = await Promise.allSettled([params, searchParams])
 
-  const resolvedParams = (
-    resolvedPageProps[0] as PromiseFulfilledResult<typeParams>
-  ).value
-  if (!resolvedParams?.params || resolvedParams.params.length < 1) {
-    notFound()
-  }
-  const paramsProduct_type = resolvedParams.params[0]!
-  const paramsProduct_version = resolvedParams.params[1]
+//   const resolvedParams = (
+//     resolvedPageProps[0] as PromiseFulfilledResult<typeParams>
+//   ).value
+//   if (!resolvedParams?.params || resolvedParams.params.length < 1) {
+//     notFound()
+//   }
+//   const paramsProduct_type = resolvedParams.params[0]!
+//   const paramsProduct_version = resolvedParams.params[1]
 
-  const resolvedSearchParams = (
-    resolvedPageProps[1] as PromiseFulfilledResult<typeSearchParams>
-  ).value
-  const searchParamsProduct_color = resolvedSearchParams?.color
+//   const resolvedSearchParams = (
+//     resolvedPageProps[1] as PromiseFulfilledResult<typeSearchParams>
+//   ).value
+//   const searchParamsProduct_color = resolvedSearchParams?.color
 
-  const resolved = await Promise.allSettled([
-    getProductTypesCached(),
-    getVariantsCached(),
-    getPagesCached(),
-  ])
-  if (resolved[0].status === 'rejected') {
-    console.error(resolved[0].reason)
-    redirect(`${ROUTE_ERROR}?message=${errorPostgres}`)
-  }
-  if (resolved[1].status === 'rejected') {
-    console.error(resolved[1].reason)
-    redirect(`${ROUTE_ERROR}?message=${errorPostgres}`)
-  }
-  if (resolved[2].status === 'rejected') {
-    console.error(resolved[2].reason)
-    redirect(`${ROUTE_ERROR}?message=${errorPostgres}`)
-  }
+//   const resolved = await Promise.allSettled([
+//     getProductTypesCached(),
+//     getVariantsCached(),
+//     getPagesCached(),
+//   ])
+//   if (resolved[0].status === 'rejected') {
+//     console.error(resolved[0].reason)
+//     redirect(`${ROUTE_ERROR}?message=${errorPostgres}`)
+//   }
+//   if (resolved[1].status === 'rejected') {
+//     console.error(resolved[1].reason)
+//     redirect(`${ROUTE_ERROR}?message=${errorPostgres}`)
+//   }
+//   if (resolved[2].status === 'rejected') {
+//     console.error(resolved[2].reason)
+//     redirect(`${ROUTE_ERROR}?message=${errorPostgres}`)
+//   }
 
-  const postgresVariants = resolved[1].value
-    .filter((variant) => variant.product_type === paramsProduct_type)
-    .sort(({ name: aName }, { name: bName }) => {
-      if (aName === 'Επίλεξε μηχανή') {
-        return -1
-      }
-      if (bName === 'Επίλεξε μηχανή') {
-        return 1
-      }
-      if (aName === specialVariant) {
-        return 1
-      }
-      if (bName === specialVariant) {
-        return -1
-      }
-      return aName.localeCompare(bName)
-    })
-  if (
-    !postgresVariants.find(
-      (variant) => variant.product_type === paramsProduct_type,
-    )
-  ) {
-    notFound()
-  }
+//   const postgresVariants = resolved[1].value
+//     .filter((variant) => variant.product_type === paramsProduct_type)
+//     .sort(({ name: aName }, { name: bName }) => {
+//       if (aName === 'Επίλεξε μηχανή') {
+//         return -1
+//       }
+//       if (bName === 'Επίλεξε μηχανή') {
+//         return 1
+//       }
+//       if (aName === specialVariant) {
+//         return 1
+//       }
+//       if (bName === specialVariant) {
+//         return -1
+//       }
+//       return aName.localeCompare(bName)
+//     })
+//   if (
+//     !postgresVariants.find(
+//       (variant) => variant.product_type === paramsProduct_type,
+//     )
+//   ) {
+//     notFound()
+//   }
 
-  const paramsVariant = paramsProduct_version
-    ? postgresVariants.find((v) =>
-        searchParamsProduct_color
-          ? v.name === paramsProduct_version &&
-            v.color === searchParamsProduct_color
-          : v.name === paramsProduct_version,
-      ) ?? undefined
-    : undefined
+//   const paramsVariant = paramsProduct_version
+//     ? postgresVariants.find((v) =>
+//         searchParamsProduct_color
+//           ? v.name === paramsProduct_version &&
+//             v.color === searchParamsProduct_color
+//           : v.name === paramsProduct_version,
+//       ) ?? undefined
+//     : undefined
 
-  const title = `${
-    paramsVariant
-      ? `${paramsProduct_type} - ${paramsVariant.name}`
-      : paramsProduct_type
-  }`
-  const description = resolved[2].value.find(
-    (page) => page.product_type === paramsProduct_type,
-  )!.product_description
-  const canonical = `/product/${decodeURIComponent(paramsProduct_type)}/${
-    paramsProduct_version ? decodeURIComponent(paramsProduct_version) : ''
-  }${`${
-    searchParamsProduct_color
-      ? `?color=${decodeURIComponent(searchParamsProduct_color)}`
-      : ''
-  }`}`
-  const productDescription = paramsVariant
-    ? paramsVariant.description
-    : description
+//   const title = `${
+//     paramsVariant
+//       ? `${paramsProduct_type} - ${paramsVariant.name}`
+//       : paramsProduct_type
+//   }`
+//   const description = resolved[2].value.find(
+//     (page) => page.product_type === paramsProduct_type,
+//   )!.product_description
+//   const canonical = `/product/${decodeURIComponent(paramsProduct_type)}/${
+//     paramsProduct_version ? decodeURIComponent(paramsProduct_version) : ''
+//   }${`${
+//     searchParamsProduct_color
+//       ? `?color=${decodeURIComponent(searchParamsProduct_color)}`
+//       : ''
+//   }`}`
+//   const productDescription = paramsVariant
+//     ? paramsVariant.description
+//     : description
 
-  const firstProductImageFileName = postgresVariants[0].images[0]
-  const firstProductImageUrl = `${
-    envServer.MINIO_PUBLIC_URL
-  }/${decodeURIComponent(paramsProduct_type)}/${decodeURIComponent(
-    firstProductImageFileName,
-  )}`
+//   const firstProductImageFileName = postgresVariants[0].images[0]
+//   const firstProductImageUrl = `${
+//     envServer.MINIO_PUBLIC_URL
+//   }/${decodeURIComponent(paramsProduct_type)}/${decodeURIComponent(
+//     firstProductImageFileName,
+//   )}`
 
-  return {
-    title: title,
-    description: description,
-    alternates: { canonical: canonical },
-    openGraph: {
-      url: canonical,
-      title: title,
-      description: productDescription,
-      images: [
-        { url: firstProductImageUrl, width: 1200, height: 630, alt: title },
-      ],
-    },
-    robots: { index: true, follow: true },
-  }
-}
+//   return {
+//     title: title,
+//     description: description,
+//     alternates: { canonical: canonical },
+//     openGraph: {
+//       url: canonical,
+//       title: title,
+//       description: productDescription,
+//       images: [
+//         { url: firstProductImageUrl, width: 1200, height: 630, alt: title },
+//       ],
+//     },
+//     robots: { index: true, follow: true },
+//   }
+// }
 
 export default async function ProductPage({
   params,
