4d99caf453c05299ceb1104c51d42d7211ba1c07
Alex Passalis <alexpassalis9@gmail.com>
2025-10-16 14:11:41 +0300

    feat: Added a free shipping banner + some checkout copy changes.

--- services/nextjs/src/components/Header.tsx
+++ services/nextjs/src/components/Header.tsx
@@ -38,71 +38,76 @@ export function Header({
   const pathname = usePathname()
 
   return (
-    <header className="relative flex justify-between items-center w-full p-2">
-      <div className="flex justify-start">
-        <button
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="flex justify-center items-center h-10 w-10 xl:scale-110 rounded-md border border-[var(--mantine-border)] transition-colors hover:cursor-pointer group"
-        >
-          <AiOutlineMenu className="transition-transform duration-200 ease-in-out group-hover:scale-110" />
-        </button>
-      </div>
-
-      <div className="flex justify-center">
-        <Link
-          href={ROUTE_HOME}
-          onClick={(e) => {
-            if (pathname === ROUTE_HOME) {
-              e.preventDefault()
-            }
-          }}
-          className={pathname === ROUTE_HOME ? 'cursor-default' : undefined}
-        >
-          <Image
-            component={NextImage}
-            src="/motowear.png"
-            width={200}
-            height={50}
-            alt="motowear.gr"
-            className="xl:scale-110"
-          />
-        </Link>
-      </div>
-
-      <div className="flex justify-end gap-2 xl:gap-4">
-        <button
-          onClick={() => setIsSearchOpen(!isSearchOpen)}
-          className="flex justify-center items-center h-10 w-10 xl:scale-110 rounded-md border border-[var(--mantine-border)] transition-colors hover:cursor-pointer group"
-        >
-          <HiMagnifyingGlass className="transition-transform duration-200 ease-in-out group-hover:scale-110" />
-        </button>
-
-        {cart.length > 0 ? (
-          <Indicator
-            radius="xl"
-            position="bottom-start"
-            color="red"
-            inline
-            label={cart.length}
-            size={15}
-            zIndex={50}
+    <>
+      <header className="relative flex justify-between items-center w-full p-2 border-b border-[var(--mantine-border)]">
+        <div className="flex justify-start">
+          <button
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            className="flex justify-center items-center h-10 w-10 xl:scale-110 rounded-md border border-[var(--mantine-border)] transition-colors hover:cursor-pointer group"
           >
+            <AiOutlineMenu className="transition-transform duration-200 ease-in-out group-hover:scale-110" />
+          </button>
+        </div>
+
+        <div className="flex justify-center">
+          <Link
+            href={ROUTE_HOME}
+            onClick={(e) => {
+              if (pathname === ROUTE_HOME) {
+                e.preventDefault()
+              }
+            }}
+            className={pathname === ROUTE_HOME ? 'cursor-default' : undefined}
+          >
+            <Image
+              component={NextImage}
+              src="/motowear.png"
+              width={200}
+              height={50}
+              alt="motowear.gr"
+              className="xl:scale-110"
+            />
+          </Link>
+        </div>
+
+        <div className="flex justify-end gap-2 xl:gap-4">
+          <button
+            onClick={() => setIsSearchOpen(!isSearchOpen)}
+            className="flex justify-center items-center h-10 w-10 xl:scale-110 rounded-md border border-[var(--mantine-border)] transition-colors hover:cursor-pointer group"
+          >
+            <HiMagnifyingGlass className="transition-transform duration-200 ease-in-out group-hover:scale-110" />
+          </button>
+
+          {cart.length > 0 ? (
+            <Indicator
+              radius="xl"
+              position="bottom-start"
+              color="red"
+              inline
+              label={cart.length}
+              size={15}
+              zIndex={50}
+            >
+              <button
+                onClick={() => setIsCartOpen(!isCartOpen)}
+                className="relative flex justify-center items-center h-10 w-10 xl:scale-110 rounded-md border border-[var(--mantine-border)] transition-colors hover:cursor-pointer group"
+              >
+                <AiFillShopping className="transition-transform duration-200 ease-in-out group-hover:scale-110" />
+              </button>
+            </Indicator>
+          ) : (
             <button
               onClick={() => setIsCartOpen(!isCartOpen)}
               className="relative flex justify-center items-center h-10 w-10 xl:scale-110 rounded-md border border-[var(--mantine-border)] transition-colors hover:cursor-pointer group"
             >
-              <AiFillShopping className="transition-transform duration-200 ease-in-out group-hover:scale-110" />
+              <AiOutlineShopping className="transition-transform duration-200 ease-in-out group-hover:scale-110" />
             </button>
-          </Indicator>
-        ) : (
-          <button
-            onClick={() => setIsCartOpen(!isCartOpen)}
-            className="relative flex justify-center items-center h-10 w-10 xl:scale-110 rounded-md border border-[var(--mantine-border)] transition-colors hover:cursor-pointer group"
-          >
-            <AiOutlineShopping className="transition-transform duration-200 ease-in-out group-hover:scale-110" />
-          </button>
-        )}
-      </div>
-    </header>
+          )}
+        </div>
+      </header>
+      <section className="bg-red-600 py-2 mb-2">
+        <h1 className="text-white text-center">ΜΕ ΚΑΡΤΑ, ΔΩΡΕΑΝ ΜΕΤΑΦΟΡΙΚΑ</h1>
+      </section>
+    </>
   )
 }
