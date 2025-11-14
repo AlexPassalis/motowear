103cc03dc5a486d3ee14c9bf75d4b52b9e2e5235
Alex Passalis <alexpassalis9@gmail.com>
2025-11-14 08:59:23 +0100

    fix: Update global.d.ts

--- services/nextjs/src/data/global.d.ts
+++ services/nextjs/src/data/global.d.ts
@@ -1,9 +1,11 @@
+import type { Pool } from 'pg'
 import type { drizzle } from 'drizzle-orm/node-postgres'
 import type Redis from 'ioredis'
 import type { Telegraf, Context, Update } from 'telegraf'
 import type { CronJob } from 'cron'
 
 declare global {
+  var global_postgres_pool: Pool | undefined
   var global_postgres: ReturnType<typeof drizzle> | undefined
   var global_redis: Redis | undefined
   var global_telegram_bot: Telegraf<Context<Update>> | undefined
